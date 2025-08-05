7358685328b6ca7facd6b2cb515984cc48c98e4c
Everson dos Santos Pinheiro <79765901+everson-s@users.noreply.github.com>
2025-08-05 09:42:42 -0300

    Filtro
    Criando o filtro da aplicação

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,7 +1,25 @@
 
 const Filter = () => {
-  return (
-    <div>Filter</div>
+  return (<div className="filter">
+      <h2>Filtrar:</h2>
+      <div className="filter-options">
+        <div>
+          <p>Status:</p>
+          <select>
+            <option value="All">Todas</option>
+            <option value="Completed">Completas</option>
+            <option value="Incomplete">incompletas</option>
+          </select>
+        </div>
+      </div>
+      <div>
+        <p>Ordem alfabética:</p>
+        <button>Asc</button>
+        <button>Desc</button>
+      </div>
+
+
+  </div>
   )
 }
 
